961d20b7a69b09304e0051ecad6b544ef389d20d
Jaden Wang <jadenyjw@gmail.com>
2016-09-17 17:56:08 -0400

    Integrate steps count

--- backend/app/controllers/api/steps_controller.rb
+++ backend/app/controllers/api/steps_controller.rb
@@ -2,13 +2,12 @@ class Api::StepsController < ApplicationController
   before_action :authenticate_api_user!
 
   def step_update
-    current_api_user.steps += step_params.count
+    current_api_user.steps.count += step_params.count
   end
   def reset_steps
-    current_api_user.steps = 0
+    current_api_user.steps.count = 0
   end
-
-
+  
   private
   def step_params
     params.require(:step).permit(:count)
