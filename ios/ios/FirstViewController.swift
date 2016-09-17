dc6994fbbd9c5bcce902944a7ec5e88c6b69e46b
Elisa Kazan <elisa.kazan@gmail.com>
2016-09-17 01:03:03 -0400

    Getting started with ios

---
+++ ios/ios/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  ios
+//
+//  Created by Elisa Kazan on 2016-09-17.
+//  Copyright © 2016 HackTheNorth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("finally it begins...")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
