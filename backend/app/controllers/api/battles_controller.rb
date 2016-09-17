37a7ce104805189691fe4c1be3380d4547515624
Jaden Wang <jadenyjw@gmail.com>
2016-09-17 09:24:46 -0400

    Fix up some formatting

--- backend/app/controllers/api/battles_controller.rb
+++ backend/app/controllers/api/battles_controller.rb
@@ -1,22 +1,25 @@
 class Api::BattlesController < ApplicationController
-  before_action :authenticate_api_user!
+  #before_action :authenticate_api_user!
+
   def create
     @battle = Battle.new()
     @battle.save
   end
+
+
   def show
     @battle = Battle.find(params[:id])
-    @participants = @battle.users
-    render json: {data: {
-        id: @battle,
+    @participants = @battle.users.all
+    render json: {
         participants: @participants
-      }}
+      }
   end
 
   #def join
-  #  @user = current_api_user
+    #@battle = Battle.find(params[:id])
+    #@user = current_api_user
   #end
-  
+
   def destroy
     @battle = Battle.find(params[:id])
     @battle.destroy
