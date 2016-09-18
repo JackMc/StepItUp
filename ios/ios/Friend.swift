dc750e52b4a00f3c70608d47eef8d0f1981e3c81
Elisa Kazan <elisa.kazan@gmail.com>
2016-09-17 20:40:34 -0400

    I made things, the views maybe?

---
+++ ios/ios/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  ios
+//
+//  Created by Elisa Kazan on 2016-09-17.
+//  Copyright © 2016 HackTheNorth. All rights reserved.
+//
+
+import UIKit
+
+class Friend: NSObject {
+
+}
---
+++ ios/ios/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  ios
+//
+//  Created by Elisa Kazan on 2016-09-17.
+//  Copyright © 2016 HackTheNorth. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
